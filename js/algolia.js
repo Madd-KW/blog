57d2a7df5c7371e941efe0c5f3e94c504419c182
dai-re <dairewahandi@gmail.com>
2023-09-20 19:27:40 +0700

    push skeleton

---
+++ js/algolia.js
@@ -0,0 +1,80 @@
+(() => {
+  // ns-params:@params
+  var baseURL = "/";
+  var params = { blogroll: [{ name: "Github", url: "https://github.com/" }, { name: "Dai.", url: "https://daire.eu.org/" }], defaultcover: "https://raw.githubusercontent.com/dai-re/dai-re/main/20230919_200038.jpg", email: "me@daire.eu.org", showcopyright: true, showmore: true, showrss: true, socialmedia: [{ name: "Github", url: "https://github.com/dai-re" }, { name: "Twitter", url: "https://twitter.com/03ii00" }, { name: "Facebook", url: "https://facebook.com/daire.it" }, { name: "Instagram", url: "https://instagram.com/dei.terada" }] };
+
+  // <stdin>
+  var { appid, appkey, searchindex: indexName, enabled } = params.algolia;
+  var searchClient = algoliasearch(appid, appkey);
+  var { autocomplete, getAlgoliaResults } = window["@algolia/autocomplete-js"];
+  function initAlgolia() {
+    autocomplete({
+      container: "#autocomplete",
+      getSources({ query }) {
+        return [
+          {
+            sourceId: "products",
+            getItems() {
+              return getAlgoliaResults({
+                searchClient,
+                queries: [
+                  {
+                    indexName,
+                    query,
+                    params: {
+                      attributesToSnippet: ["name:10", "description:35"]
+                    }
+                  }
+                ]
+              });
+            },
+            templates: {
+              item({ item, components, html }) {
+                return html`<a class="aa-ItemWrapper" href="${baseURL}${item.uri}">
+                <div class="aa-ItemContent">
+                  <div class="aa-ItemContentBody">
+                    <div class="aa-ItemContentTitle">
+                      ${components.Highlight({
+                  hit: item,
+                  attribute: "name"
+                })}
+                    </div>
+                    <div class="aa-ItemContentDescription">
+                      ${components.Snippet({
+                  hit: item,
+                  attribute: "description"
+                })}
+                    </div>
+                  </div>
+                  <div class="aa-ItemActions">
+                    <button
+                      class="aa-ItemActionButton aa-DesktopOnly aa-ActiveOnly"
+                      type="button"
+                      title="Select"
+                    >
+                      <svg
+                        viewBox="0 0 24 24"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                      >
+                        <path
+                          d="M18.984 6.984h2.016v6h-15.188l3.609 3.609-1.406 1.406-6-6 6-6 1.406 1.406-3.609 3.609h13.172v-4.031z"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+              </a>`;
+              }
+            }
+          }
+        ];
+      }
+    });
+    document.querySelector("#autocomplete input").focus();
+  }
+  if (enabled) {
+    initAlgolia();
+  }
+})();
